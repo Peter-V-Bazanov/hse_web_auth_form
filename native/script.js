bda3509a464de284dec36e4b48a1994dcdd19ee1
Peter V. Bazanov <peter.v.bazanov@gmail.com>
2025-03-27 16:39:28 +0500

    добавил функцию применения языка

--- native/script.js
+++ native/script.js
@@ -16,4 +16,28 @@ document.getElementById('togglePassword').addEventListener('click', function () 
     return await response.json();
 }
  
+async function applyLanguage(lang) {
+  const translations = await loadLanguage(lang);
+
+  // Заменяем текстовые значения
+  document.querySelectorAll('[data-i18n]').forEach(el => {
+      const key = el.getAttribute('data-i18n');
+      if (translations[key]) {
+          el.innerText = translations[key];
+      }
+  });
+
+  // Заменяем значения placeholder
+  document.querySelectorAll('[data-i18n-placeholder]').forEach(el => {
+      const key = el.getAttribute('data-i18n-placeholder');
+      if (translations[key]) {
+          el.placeholder = translations[key];
+      }
+  });
+
+  localStorage.setItem('language', lang);
+}
+
+
+
   
